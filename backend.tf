e01c99d6063f256e504d0e99bf0e069882e44b20
Enrique Delgado <kikeead@gmail.com>
2024-05-03 16:14:48 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,21 +1,8 @@
-resource "aws_s3_bucket" "S3_tf" {
-  bucket = "s3_desde_GHA"  # Nombre único para tu bucket
-  acl    = "private"    # Control de acceso (ajústalo según tus necesidades)
-  tags = {
-    Name = "Soy un bucket generado desde Terraform"
-  }
-}
-
-resource "aws_dynamodb_table" "dynamodb_tf" {
-  name           = "dynamo_desde_tf"
-  billing_mode   = "PAY_PER_REQUEST"  # Modelo de facturación (ajústalo según tus necesidades)
-  hash_key       = "LockID"              # Clave de hash (puedes cambiarlo según tus necesidades)
-  attribute {
-    name = "LockID"
-    type = "S"  # Tipo de atributo (N para número, S para cadena, etc.)
-  }
-
-  tags = {
-    Name = " Soy DynamoDB generada desde Terraform"
+terraform {
+  backend "s3" {
+    bucket                 = "s3_desde_GHA"
+    region                 = "us-east-1"
+    key                    = "backend.tfstate"
+    dynamodb_table         = "terraformstatelock-rem"
   }
 }
